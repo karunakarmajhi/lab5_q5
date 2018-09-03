f8543a12753f8ccb5c7bd2f7fef2ff1ede08fcfd
karunakarmajhi <42205167+karunakarmajhi@users.noreply.github.com>
2018-09-03 14:03:06 +0530

    Add files via upload

---
+++ lab5_q5.cpp
@@ -0,0 +1,24 @@
+//library
+#include<iostream>
+using namespace std;
+//defining the main function
+int main(){
+//defining the variables
+int a;
+//display
+cout<<"enter the value of a"<<end l;
+//storing value of a
+cin>>a;
+//conditional statement if
+if (a%2==0){
+//if condition is true
+cout<<"a is even"<<endl;
+}
+//conditional statement else
+else{
+//if first condition is false
+cout<<"a is odd";
+}
+return 0;
+}
+
